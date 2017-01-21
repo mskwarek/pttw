01522244d589d4399488b4d606ba6bc19acb5e81
Marcin Skwarek <marcin.skwarek@mcx.pl>
2017-01-21 23:09:13 +0100

    * demux
    Change-Id: Ia68a7ca0e1f9197cb9ba9aeb333b9f89ad95c20a

--- start.bat
+++ start.bat
@@ -7,4 +7,8 @@
 ::call 05_ofdm_mod\
 call 06_awgn\awgn.exe --src input_awgn --dst output_awgn --snr 30
 ::call 07_fast_fading\
+::call 05_ofdm_mod\
+::call 04_qpsk_mod\
+::call 03_spreader\
+::call 02_coder\
 ::call 01_binary_src_dst\
