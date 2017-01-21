21c7b6ad9d132ffb97fbceabfcb454a782d42219
Marcin Skwarek <marcin.skwarek@mcx.pl>
2017-01-21 22:53:15 +0100

    * awgn simulation
    Change-Id: Ia7709aa53c75e51865a5c8bb10c460557dc77103

---
+++ start.bat
@@ -0,0 +1,10 @@
+@echo on
+
+::call 01_binary_src_dst\
+::call 02_coder\
+::call 03_spreader\
+::call 04_qpsk_mod\
+::call 05_ofdm_mod\
+call 06_awgn\awgn.exe --src input_awgn --dst output_awgn --snr 30
+::call 07_fast_fading\
+::call 01_binary_src_dst\
